b50563b6b7eb3b03fb5688b74918ee53e0f11076
marayatdev <149398037+marayatdev@users.noreply.github.com>
2025-05-24 22:58:14 +0700

    chore: update and debug config for deploy.

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,7 @@ class App {
   private setMiddlewares(): void {
     this.app.use(
       cors({
-        origin: "http://172.20.112.1:3000", // หรือ frontend ของคุณ
+        origin: "http://20.194.193.147:3000", // หรือ frontend ของคุณ
         credentials: true,
       })
     );
